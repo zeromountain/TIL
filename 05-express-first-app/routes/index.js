929fb604680c95408df03c14f91c9a6e8931c7f9
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-03-19 22:11:21 +0900

    express project structure and First little Appp
    라우터를 활용한 분기

--- 05-express-first-app/routes/index.js
+++ 05-express-first-app/routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+// localhost:8000/users/detail
+router.get('/users/detail', function(req, res, next) {
+  res.send('detail');
+});
 
 module.exports = router;
